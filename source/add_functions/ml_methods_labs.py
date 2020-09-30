0f1ae1db40e4c2d32c736752249a7f1339c20636
marvande <marijn.vandermeer@epfl.ch>
2020-09-30 17:50:29 +0200

    last methods

--- source/add_functions/ml_methods_labs.py
+++ source/add_functions/ml_methods_labs.py
@@ -1,26 +1,5 @@
 import numpy as np
 
-def compute_gradient(y, tx, w):
-    return (-1/len(y))*tx.T@(y-tx@w)
-
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """Gradient descent algorithm."""
-    # Define parameters to store w and loss
-    w = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        grad = compute_gradient(y, tx, w)
-        loss = MSE_compute_loss(y, tx, w)
-        w -= gamma*grad
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-
-    return losses, ws
-
 def grid_search(y, tx, w0, w1):
     """Algorithm for grid search."""
     losses = np.zeros((len(w0), len(w1)))
@@ -30,35 +9,13 @@ def grid_search(y, tx, w0, w1):
             losses[i,j] = compute_loss(y, tx, w)
     return losses
 
-def MSE_compute_loss(y, tx, w):
-    """Calculate the loss.
-    You can calculate the loss using mse or mae.
-    """
-    MSE = np.square(np.subtract(y,tx@w)).mean()
-    return MSE
-
-def compute_stoch_gradient(y, tx, w):
-    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
-    return (-1/len(y))*tx.T@(y-tx@w)
-
 def standardize(x):
     centered_data = x - np.mean(x, axis=0)
     std_data = centered_data / np.std(centered_data, axis=0)
     return std_data
 
-def stochastic_gradient_descent(
-        y, tx, initial_w, batch_size, max_iters, gamma):
-    """Stochastic gradient descent algorithm."""
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
-            grad = compute_gradient(minibatch_y, minibatch_tx, w)
-            loss = compute_loss(minibatch_y, minibatch_tx, w)
-            w -= gamma*grad
-            # store w and loss
-            ws.append(w)
-            losses.append(loss)
-    return losses, ws
+
+def standardize_with_meanstd(x, mean, std):
+    centered_data = x - mean
+    std_data = centered_data / std
+    return std_data
