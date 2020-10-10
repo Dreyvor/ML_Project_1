8af1d3bff6403727ab640cec021eb66927f2d943
marvande <marijn.vandermeer@epfl.ch>
2020-10-10 14:20:38 +0200

    feature expansioon

--- source/add_functions/ml_methods_labs.py
+++ source/add_functions/ml_methods_labs.py
@@ -10,11 +10,21 @@ def grid_search(y, tx, w0, w1):
     return losses
 
 def standardize(x):
-    centered_data = x - np.mean(x, axis=0)
-    std_data = centered_data / np.std(centered_data, axis=0)
-    return std_data
+    # if bias column: 
+    if np.array_equal(x[:, 0], np.ones(len(x))):
+        centered_data = x[:,1:] - np.mean(x[:,1:], axis=0)
+        std_data = centered_data / np.std(centered_data, axis=0)
+        return np.hstack((np.ones((len(x),1)),std_data))
+    else:
+        centered_data = x - np.mean(x, axis=0)
+        std_data = centered_data / np.std(centered_data, axis=0)
+        return std_data
 
-def standardize_with_meanstd(x, mean, std):
-    centered_data = x - mean
-    std_data = centered_data / std
-    return std_data
+def standardize_with_mean_std(x, mean, std):
+    # if bias term:
+    if np.array_equal(x[:, 0], np.ones(len(x))):
+        std_data = (x[:,1:] - mean)/std
+        return np.hstack((np.ones((len(x),1)), std_data))
+    else:
+        std_data = (x - mean)/std
+        return std_data
